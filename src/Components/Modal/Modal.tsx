7bc0af5bc16d803d22af373ea403c4b615790f97
Your Name <you@example.com>
2025-05-26 21:49:26 +0400

    modal

--- src/Components/Modal/Modal.tsx
+++ src/Components/Modal/Modal.tsx
@@ -6,8 +6,8 @@ closeModal:()=>void
 
 export const Modal:FC<ModalProps> = ({closeModal}) => {
     return(
-        <div>
-            <img src="" alt="" />
+        <div style={{width:1000,height:200,border:'2px solid black',backgroundColor:'red'}}>
+            <img style={{width:1000,height:200}} src="https://gamemag.ru/images/cache/News/News177754/02b3b0eeae-2_2780x1200.jpg" alt="" />
             <button onClick={closeModal}>Закрыть окно</button>
         </div>
     )
